f3c4f35cf712cfa6cd8b71f4f536161232aefe86
marco <marco.nunes.senac@gmail.com>
2024-05-08 15:29:07 -0300

    8/5

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,10 @@ import app from './server'
 
 const PORT = process.env.PORT || 4003
 
+process.on('uncaughtException', (err)=>{
+  console.log(err)
+})
+
 app.listen(PORT, () => {
   console.log(`The server is running on port ${PORT}`)
 })
